b6a4dd79ca5e86e1ee637a3f8196d46caf737406
Vaibhav099-pattar <vaibhavpattar199@gmail.com>
2025-04-10 12:08:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_terraform"
+  image = docker_image.nginx.name  # Fixed the reference here
+
+  ports {
+    internal = 80
+    external = 8081  # Changed port number here
+  }
+}
